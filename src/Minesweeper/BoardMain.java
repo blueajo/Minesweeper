44a4070da344481b56e497064d511f18aa44e1bc
Sravan Konda <sravansaikonda@gmail.com>
2016-12-01 09:17:27 -0800

    Test

--- src/Minesweeper/BoardMain.java
+++ src/Minesweeper/BoardMain.java
@@ -3,8 +3,10 @@ package Minesweeper;
 public class BoardMain {
 	
 	public static void main(String[] args) {
-		Board board1 = new Board();
-		board1.pack();
-		board1.setVisible(true);
+		Board board1 = new Board(5, 5);
+		System.out.println(board1);
+		
+		// board1.pack();
+		// board1.setVisible(true);
 	}
 }
