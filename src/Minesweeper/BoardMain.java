6effb5bcbc73b26b59d917584ea1fde29a0612e0
Blue Jo <blueajo@gmail.com>
2016-12-19 18:52:44 -0800

    added rudimentary gui

--- src/Minesweeper/BoardMain.java
+++ src/Minesweeper/BoardMain.java
@@ -4,9 +4,5 @@ public class BoardMain {
 	
 	public static void main(String[] args) {
 		Board board1 = new Board(5, 5);
-		System.out.println(board1);
-		
-		// board1.pack();
-		// board1.setVisible(true);
 	}
 }
